d3bcdb1af1e371e8c4078759c70ca5664c03f9fa
oeway <oeway007@gmail.com>
2021-11-30 21:56:34 +0100

    Add check-pending items

---
+++ scripts/check_pending.js
@@ -0,0 +1,29 @@
+import { readFile } from "fs/promises";
+import yaml from "js-yaml";
+
+const indexRdf = "./rdf.yaml";
+
+async function main() {
+  const templateStr = await readFile(indexRdf);
+  const newIndexRdf = yaml.load(templateStr);
+  const currentItems = newIndexRdf.attachments.zenodo;
+  const passedItems = currentItems.filter(item => item.status === "passed");
+  const pendingItems = currentItems.filter(item => item.status === "pending");
+  console.log(
+    "Passed rdf items",
+    passedItems.map(item => item.id)
+  );
+  if(pendingItems.length > 0) {
+    console.log(
+      "ERROR: There are still pending rdf items: ",
+      pendingItems.map(item => item.id)
+    );
+    console.log("Please resolve all the pending items in the rdf.yaml!!!")
+    // Fail the test
+    process.exit(1);
+  }
+  else{
+    console.log("No pending rdf items");
+  }
+}
+main();
