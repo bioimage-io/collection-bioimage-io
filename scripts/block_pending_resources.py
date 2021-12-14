ab04cbd6b4445b830d25c1a4cb42da725325606c
fynnbe <thefynnbe@gmail.com>
2021-12-14 19:29:00 +0100

    concept -> resource

--- scripts/block_pending_resources.py
+++ scripts/block_pending_resources.py
@@ -22,15 +22,15 @@ def main(
     if branch.startswith("auto-update-"):
         id_ = branch[len("auto-update-") :]
 
-        concept_path = collection_folder / id_ / "concept.yaml"
-        concept = yaml.load(concept_path)
-        for v in concept["versions"]:
+        resource_path = collection_folder / id_ / "resource.yaml"
+        resource = yaml.load(resource_path)
+        for v in resource["versions"]:
             if v["status"] == "pending":
                 v["status"] = "blocked"
                 made_changes = True
 
         if made_changes:
-            yaml.dump(concept, concept_path)
+            yaml.dump(resource, resource_path)
 
     else:
         # don't fail, but warn for non-auto-update branches
