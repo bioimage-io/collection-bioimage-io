b3b3401d08cb7f880f49c4da3ba8945686076397
oeway <oeway007@gmail.com>
2021-12-27 02:46:53 +0100

    Fix get pending check

--- scripts/get_pending.py
+++ scripts/get_pending.py
@@ -1,10 +1,12 @@
 import json
 from pathlib import Path
 
+import requests
 import typer
 from ruamel.yaml import YAML
 
 yaml = YAML(typ="safe")
+MAIN_BRANCH_URL = "https://raw.githubusercontent.com/bioimage-io/collection-bioimage-io/main"
 
 
 def set_gh_actions_output(name: str, output: str):
@@ -23,13 +25,23 @@ def main(
     pending = []
     if branch.startswith("auto-update-"):
         resource_id = branch[len("auto-update-") :]
-
         resource_path = collection_folder / resource_id / "resource.yaml"
+        response = requests.get(f"{MAIN_BRANCH_URL}/{resource_path}")
+        if response.ok:
+            previous_resource = yaml.load(response.text)
+            previous_versions = {v["version_id"]: v for v in previous_resource["versions"]}
+        else:
+            previous_resource = None
+            previous_versions = None
         resource = yaml.load(resource_path)
-        for v in resource["versions"]:
-            if v["status"] == "pending":
-                pending.append(v["version_id"])
-
+        # status of the entire resource item has changed
+        if previous_resource and previous_resource.get("status") != resource.get("status"):
+            pending = resource["versions"]
+        else:
+            for v in resource["versions"]:
+                previous_version = previous_versions and previous_versions.get(v["version_id"])
+                if previous_version is None or previous_version != v: # check changes
+                    pending.append(v["version_id"])
     else:
         # don't fail, but warn for non-auto-update branches
         print(f"called with non-auto-update branch {branch}")
