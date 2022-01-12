28ef7a65a35366e98e87ed725e1d04d4e807d2f3
fynnbe <thefynnbe@gmail.com>
2022-01-13 00:43:54 +0100

    base prepare_to_deploy on pending_matrix

--- scripts/prepare_to_deploy_preview.py
+++ scripts/prepare_to_deploy_preview.py
@@ -1,64 +1,28 @@
-import json
 import shutil
 from pathlib import Path
 
 import typer
 from ruamel.yaml import YAML
 
-from bioimageio.spec import load_raw_resource_description
-from bioimageio.spec.io_ import serialize_raw_resource_description_to_dict
+from utils import iterate_over_gh_matrix
 
 yaml = YAML(typ="safe")
 
 
 def main(
-    collection_dir: Path,
-    branch: str = typer.Argument(
-        ...,
-        help="branch name should be 'auto-update-{resource_id} and is only used to get resource_id.",
-    ),
     resources_dir: Path = typer.Argument(...),
-    pending_versions: str = typer.Argument(
-        ..., help="json string of list of pending versions_ids"
-    ),
-    artifact_dir: Path = typer.Argument(
-        ..., help="folder with validation and conda environment artifacts"
-    ),
-) -> int:
-    if branch.startswith("auto-update-"):
-        resource_id = branch[len("auto-update-") :]
-    else:
-        print(f"called with non-auto-update branch {branch}")
-        return 0
-
-    resource_path = collection_dir / resource_id / "resource.yaml"
-    resource = yaml.load(resource_path)
-    resource_folder = resources_dir / resource["id"]
-    pending_versions = json.loads(pending_versions)["version_id"]
-    for v in resource["versions"]:
-        version_id = v["version_id"]
-        if version_id not in pending_versions or v["status"] == "blocked":
-            continue
-
-        try:
-            rdf_node = load_raw_resource_description(v["source"])
-        except Exception as e:
-            print(f"Failed to interpret {v['source']} as rdf: {e}")
-            rdf_data = {}
-        else:
-            rdf_data = serialize_raw_resource_description_to_dict(rdf_node)
-
-        rdf_data.update(v)
-
-        version_folder = resource_folder / version_id
-        version_folder.mkdir(parents=True, exist_ok=True)
-        yaml.dump(rdf_data, version_folder / "rdf.yaml")
+    pending_versions: str = typer.Argument(..., help="json string of list of pending versions_ids"),
+    artifact_dir: Path = typer.Argument(..., help="folder with validation and conda environment artifacts"),
+):
+    for matrix in iterate_over_gh_matrix(pending_versions):
+        resource_id = matrix["resource_id"]
+        version_id = matrix["version_id"]
 
         # move validation summaries and conda env yaml files from artifact to version_id folder
+        version_folder = resources_dir / resource_id / version_id
+        version_folder.mkdir(parents=True, exist_ok=True)
         for sp in artifact_dir.glob(f"**/{version_id.replace('/', '')}*/**/*.yaml"):
             shutil.move(str(sp), str(version_folder))
-
-    return 0
 
 
 if __name__ == "__main__":
