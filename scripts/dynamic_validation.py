f674c747420a9753365494c2aab444ede851444e
fynnbe <thefynnbe@gmail.com>
2022-01-28 17:47:43 +0100

    fall back to original rdf when validating

--- scripts/dynamic_validation.py
+++ scripts/dynamic_validation.py
@@ -1,11 +1,11 @@
 from pathlib import Path
+from typing import Optional
 
 import typer
 
 from bioimageio.core.resource_tests import test_resource
 from bioimageio.spec.shared import yaml
-
-from typing import Optional
+from utils import get_rdf_source
 
 try:
     from typing import get_args
@@ -20,13 +20,15 @@ def main(
     resource_id: str,
     version_id: str,
     weight_format: Optional[str] = typer.Argument(None, help="weight format to test model with."),
+    collection_dir: Path = Path(__file__).parent / "../collection",
 ):
     if weight_format is None:
         # no dynamic tests for non-model resources...
         return
 
     summary_path = dist / resource_id / version_id / weight_format / f"validation_summary_{weight_format}.yaml"
-    rdf_source = f"{SOURCE_BASE_URL}/resources/{resource_id}/{version_id}/rdf.yaml"
+
+    rdf_source = get_rdf_source(collection_dir=collection_dir, resource_id=resource_id, version_id=version_id)
 
     summary = test_resource(rdf_source, weight_format=weight_format)
     if "name" not in summary:  # todo: remove, summaries of the future always have a name
