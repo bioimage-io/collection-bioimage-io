7a25f6d17181a7e8f65c9b115e0896911767afa5
fynnbe <thefynnbe@gmail.com>
2022-01-31 13:24:54 +0100

    merge get_pending into get_pending_validations

--- scripts/get_pending_validations.py
+++ scripts/get_pending_validations.py
@@ -1,5 +1,5 @@
-import warnings
 from pathlib import Path
+from typing import Optional
 
 import typer
 from ruamel.yaml import YAML
@@ -12,12 +12,25 @@ yaml = YAML(typ="safe")
 def main(
     collection_dir: Path = Path(__file__).parent / "../collection",
     gh_pages_dir: Path = Path(__file__).parent / "../gh-pages",
+    branch: Optional[str] = typer.Option(
+        None,
+        help="(used in auto-update PR) If branch is 'auto-update-{resource_id} it is used to get resource_id and limit validation to unvalidated versions of that resource.",
+    ),
 ):
     """output all accepted resource versions that are missing validation"""
-    pending = []
-    collection_resources = [yaml.load(r_path) for r_path in collection_dir.glob("**/resource.yaml")]
-    partner_resources = [yaml.load(r_path) for r_path in (gh_pages_dir / "partner_collection").glob("**/resource.yaml")]
+
+    if branch is not None and branch.startswith("auto-update-"):
+        resource_id = branch[len("auto-update-") :]
+    else:
+        resource_id = "**"
+
+    collection_resources = [yaml.load(r_path) for r_path in collection_dir.glob(f"{resource_id}/resource.yaml")]
+    partner_resources = [
+        yaml.load(r_path) for r_path in (gh_pages_dir / "partner_collection").glob(f"{resource_id}/resource.yaml")
+    ]
     known_resources = [r for r in collection_resources + partner_resources if r["status"] == "accepted"]
+
+    pending = []
     for r in known_resources:
         resource_id = r["id"]
         for v in r["versions"]:
@@ -30,9 +43,8 @@ def main(
                 rdf = yaml.load(rdf_path)  # deployed RDF may already have test_summary
                 is_pending = "test_summary" not in rdf.get("config", {}).get("bioimageio", {})
             else:
-                # RDF not yet deployed?!
-                warnings.warn(f"skip validating missing rdf: {rdf_path}")
-                is_pending = False
+                # in PR RDF is not deployed (yet)
+                is_pending = True
 
             if is_pending:
                 pending.append((resource_id, version_id))
