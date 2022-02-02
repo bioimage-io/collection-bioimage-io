641ca5129f61b262f688a54b11c9c8339bdef6f0
fynnbe <thefynnbe@gmail.com>
2022-02-02 16:15:17 +0100

    improve run local script

--- scripts/run_main_ci_equivalent_local.py
+++ scripts/run_main_ci_equivalent_local.py
@@ -1,5 +1,4 @@
 """script to run a rough equivalent of the github actions workflow 'collection_main.yaml' locally"""
-import os
 import shutil
 import subprocess
 from pathlib import Path
@@ -7,20 +6,25 @@ from pprint import pprint
 
 import typer
 
+from deploy_test_summaries import main as deploy_test_summaries_script
 from dynamic_validation import main as dynamic_validation_script
-from get_pending_validations import main as get_pending_validations_script
 from generate_collection_rdf import main as generate_collection_rdf_script
+from get_pending_validations import main as get_pending_validations_script
+from static_validation import main as static_validation_script
 from update_external_resources import main as update_external_resources_script
 from update_partner_resources import main as update_partner_resources_script
-from deploy_test_summaries import main as deploy_test_summaries_script
-from static_validation import main as static_validation_script
-from utils import iterate_over_gh_matrix
 from update_resource_rdfs import main as update_resource_rdfs_script
+from utils import iterate_over_gh_matrix
 
 
 def fake_deploy(dist: Path, deploy_to: Path):
     shutil.copytree(str(dist), str(deploy_to), dirs_exist_ok=True)
     shutil.rmtree(str(dist))
+
+
+def end_of_step():
+    if input("Continue?([y]/n)").lower().startswith("n"):
+        raise RuntimeError("abort")
 
 
 def main(
@@ -44,10 +48,15 @@ def main(
         rdf_template_path=rdf_template_path,
         current_collection_format=current_collection_format,
     )
+    fake_deploy(dist, gh_pages)
+
     updates = update_external_resources_script(collection_dir=collection_dir)
     print("would open auto-update PRs with:")
     pprint(updates)
 
+    fake_deploy(dist, collection_dir)
+
+    end_of_step()
     #########################
     # get pending validations
     #########################
@@ -61,7 +70,10 @@ def main(
 
     # create updated rdfs that don't exist yet
     pending = update_resource_rdfs_script(
-        dist=dist, pending_matrix=pending["pending_matrix"], collection_dir=collection_dir, future_deployed_path=gh_pages
+        dist=dist,
+        pending_matrix=pending["pending_matrix"],
+        collection_dir=collection_dir,
+        future_deployed_path=gh_pages,
     )
 
     print("\npending (updated):")
@@ -69,6 +81,10 @@ def main(
 
     fake_deploy(dist, gh_pages)
 
+    end_of_step()
+    ############################
+    # validate/static-validation
+    ############################
     # perform static validation for pending resources
     static_out = static_validation_script(
         dist=artifacts / "static_validation_artifact", pending_matrix=pending["pending_matrix"]
@@ -79,6 +95,10 @@ def main(
     if not static_out["has_dynamic_test_cases"]:
         return
 
+    end_of_step()
+    #############################
+    # validate/dynamic-validation
+    #############################
     for matrix in iterate_over_gh_matrix(static_out["dynamic_test_cases"]):
         print(
             f"\ndynamic validation (r: {matrix['resource_id']}, v: {matrix['version_id']}, w: {matrix['weight_format']}):"
@@ -91,12 +111,23 @@ def main(
             weight_format=matrix["weight_format"],
         )
 
+    end_of_step()
+    #################
+    # validate/deploy
+    #################
     deploy_test_summaries_script(
         dist=dist, gh_pages_dir=gh_pages, pending_versions=pending["pending_matrix"], artifact_dir=artifacts
     )
 
     fake_deploy(dist, gh_pages)
+
+    end_of_step()
+    ##################
+    # build-collection
+    ##################
     generate_collection_rdf_script(collection_dir=collection_dir, dist=dist)
+
+    fake_deploy(dist, gh_pages)
 
 
 if __name__ == "__main__":
