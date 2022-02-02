2f1eb3281b30dffbd694273ac54d88887c8606d1
fynnbe <thefynnbe@gmail.com>
2022-02-02 16:15:56 +0100

    assume resource is in partner_collection if not available in collection

--- scripts/update_resource_rdfs.py
+++ scripts/update_resource_rdfs.py
@@ -10,6 +10,7 @@ def main(
     dist: Path = Path(__file__).parent / "../dist",
     pending_matrix: str = "",
     collection_dir: Path = Path(__file__).parent / "../collection",
+    gh_pages: Path = Path(__file__).parent / "../gh-pages",
     future_deployed_path: Path = Path(__file__).parent
     / "../gh-pages",  # we write rdfs to dist, they won't be there in the next github actions job though...
 ) -> dict:
@@ -24,7 +25,11 @@ def main(
         version_id = matrix["version_id"]
 
         resource_path = collection_dir / resource_id / "resource.yaml"
-        assert resource_path.exists()
+        if not resource_path.exists():
+            resource_path = gh_pages / "partner_collection" / resource_id / "resource.yaml"
+
+        assert resource_path.exists(), resource_path
+
         resource = yaml.load(resource_path)
 
         # limit resource to the single version at hand
@@ -34,6 +39,7 @@ def main(
         updated_version_rdfs = update_resource_rdfs(dist=dist, resource=resource)
 
         matrix["rdf_path"] = str(future_deployed_path / updated_version_rdfs[version_id].relative_to(dist))
+
         updated_pending_include.append(matrix)
 
     out = dict(pending_matrix=dict(include=updated_pending_include), has_pending_matrix=bool(updated_pending_include))
