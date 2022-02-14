356c2bf2ede276836c6e630d04c898742bf0ad7a
fynnbe <thefynnbe@gmail.com>
2022-02-14 19:03:47 +0100

    wf dispatch with python

---
+++ scripts/trigger_partner_wf.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+
+import typer
+from github import Github
+
+
+def main(
+    pending_matrix: str,
+    *,
+    owner: str = "ilastik",
+    repo: str = "bioimage-io-resources",
+    workflow: str = "test_bioimageio_resources.yaml",
+    pat: str = None,
+):
+    inputs = dict(pending_matrix=pending_matrix)
+    ref = "main"
+    pprint(inputs)
+    g = Github(login_or_token=pat)
+    repo = g.get_repo(f"{owner}/{repo}")
+    wf = repo.get_workflow(workflow)
+    wf.create_dispatch(ref, inputs=inputs)
+
+
+if __name__ == "__main__":
+    typer.run(main)
