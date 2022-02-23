bccc8540637048aa51646d730afe428139781087
fynnbe <thefynnbe@gmail.com>
2022-02-23 16:50:48 +0100

    checkout gh-pages rather than downloading from raw.github

--- scripts/get_previous_pr_urls.py
+++ scripts/get_previous_pr_urls.py
@@ -3,22 +3,19 @@ import json
 import warnings
 from pathlib import Path
 
-import requests
-
-from bare_utils import RAW_BASE_URL, set_gh_actions_outputs
+from bare_utils import set_gh_actions_outputs
 
 
 def main(resource_id: str, pr_urls_path: Path = Path(__file__).parent / "../dist/pr_urls.json"):
-    try:
-        r = requests.get(f"{RAW_BASE_URL}/pr_urls.json")
-        r.raise_for_status()
-        pr_urls = r.json()
-    except Exception as e:
-        warnings.warn(str(e))
+    if pr_urls_path.exists():
+        with pr_urls_path.open(encoding="utf-8") as f:
+            pr_urls = json.load(f)
+    else:
+        warnings.warn(f"Didn't find pr_urls_path: {pr_urls_path}")
+        pr_urls_path.parent.mkdir(parents=True, exist_ok=True)
         pr_urls = {}
 
     # save pr_urls for adding new pr url in a following step
-    pr_urls_path.parent.mkdir(parents=True, exist_ok=True)
     with pr_urls_path.open("w", encoding="utf-8") as f:
         json.dump(pr_urls, f, ensure_ascii=False, indent=4, sort_keys=True)
 
