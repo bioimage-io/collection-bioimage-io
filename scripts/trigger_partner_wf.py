c607fa88e6da53c20f58821f9c07be68d3a936f5
fynnbe <thefynnbe@gmail.com>
2022-02-23 18:12:46 +0100

    fix trigger_partner_wf pat

--- scripts/trigger_partner_wf.py
+++ scripts/trigger_partner_wf.py
@@ -1,3 +1,4 @@
+import os
 from pprint import pprint
 
 import typer
@@ -10,12 +11,11 @@ def main(
     owner: str = "ilastik",
     repo: str = "bioimage-io-resources",
     workflow: str = "test_bioimageio_resources.yaml",
-    pat: str = None,
 ):
     inputs = dict(pending_matrix=pending_matrix)
     ref = "main"
     pprint(inputs)
-    g = Github(login_or_token=pat)
+    g = Github(login_or_token=os.environ["PAT"])
     repo = g.get_repo(f"{owner}/{repo}")
     wf = repo.get_workflow(workflow)
     success = wf.create_dispatch(ref, inputs=inputs)
